ce65859a682970cbfb414be5d4d45de7848e4f2d
marswn08 <marswn08@gmail.com>
2026-07-08 21:20:33 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-body {}
+document.querySelectorAll('a[href^="#"]').forEach(link=>{
+  link.addEventListener('click',e=>{
+    const target=document.querySelector(link.getAttribute('href'));
+    if(target){e.preventDefault();target.scrollIntoView({behavior:'smooth'});}
+  });
+});
